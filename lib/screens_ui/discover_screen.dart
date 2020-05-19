051fb46060e575d7ff615e41212fcb0b3afd3f36
Matt Creekmore <matthewacreekmore@gmail.com>
2020-05-19 15:54:14 -0400

    Removed --obfuscate tag

--- lib/screens_ui/discover_screen.dart
+++ lib/screens_ui/discover_screen.dart
@@ -3,6 +3,7 @@ import 'package:qr_flutter/qr_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:moves/store/store.dart';
 
+// actually the QR screen
 class DiscoverScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
