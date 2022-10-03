4fe014827595c50e80d06e11fef0572b044881e7
Matt Creekmore <48491595+mcreekmore@users.noreply.github.com>
2022-10-02 21:13:18 -0400

    key rotated and removed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyBhgIifdX2YAvcIUGOksAyYJM40BzITYdQ")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
